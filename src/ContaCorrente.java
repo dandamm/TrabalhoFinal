1ed05b52e6edfd702e3ac0148c31ed9c490747fe
Daniela Damo <danieladammom@gmail.com>
2017-04-02 17:56:40 -0300

    Formatando

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,21 +1,22 @@
 
 public class ContaCorrente {
-	
+
 	private double saldo, chequeEspecial = 500;
 
 	public ContaCorrente(double saldo) {
-		
+
 		this.saldo = saldo;
 	}
-
 
 	public double getSaldo() {
 		return saldo;
 	}
-	public double setChequeEspecial(double chequeEspecial){
-		this.chequeEspecial= chequeEspecial;
+
+	public double setChequeEspecial(double chequeEspecial) {
+		this.chequeEspecial = chequeEspecial;
 		return chequeEspecial;
 	}
+
 	public double getChequeEspecial() {
 		return chequeEspecial;
 	}
@@ -29,30 +30,29 @@ public class ContaCorrente {
 		if (valor <= this.saldo) {
 			this.saldo = this.saldo - valor;
 			System.out.println("Saque efetuado. Saldo atual: " + this.getSaldo());
-		} 
-		else if (valor > this.saldo && valor<=this.saldo+this.chequeEspecial) {
+		} else if (valor > this.saldo && valor <= this.saldo + this.chequeEspecial) {
 			this.chequeEspecial = this.saldo + this.chequeEspecial - valor;
 			System.out.println("Seu saldo está sendo retirado do seu Cheque Especial e você pagará 50 reais como taxa");
 			this.chequeEspecial = this.chequeEspecial - 50;
 			System.out.println("Saldo restante no cheque especial " + this.getChequeEspecial());
-			
-		} 
-		else {
+
+		} else {
 			System.out.println("Seu saldo é insuficiente.");
 		}
 	}
 
 	public void transferirContaPoupanca(double valor) {
-		if(valor<=this.saldo){
+		if (valor <= this.saldo) {
 			this.saldo = (this.saldo - valor) - 1;
-			System.out.println("Foi depositado para a conta Poupança o valor de: " + valor + " Seu saldo atual é: "  + this.getSaldo() + " reais. Você pagou 1 real em taxa de transferência!");
-		}
-		else{
+			System.out.println("Foi depositado para a conta Poupança o valor de: " + valor + " Seu saldo atual é: "
+					+ this.getSaldo() + " reais. Você pagou 1 real em taxa de transferência!");
+		} else {
 			System.out.println("Saldo insufiente. Transferência negada.");
 		}
 
 	}
-	public void verSaldo(){
+
+	public void verSaldo() {
 		System.out.println(getSaldo());
 	}
 
--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,11 +1,12 @@
 
 public class ContaPoupanca {
-	
+
 	private double saldo;
 
 	public ContaPoupanca(double saldo) {
 		this.saldo = saldo;
 	}
+
 	public double getSaldo() {
 		return saldo;
 	}
@@ -25,16 +26,17 @@ public class ContaPoupanca {
 	}
 
 	public void transferirContaCorrente(double valor) {
-		if(valor<=this.saldo){
+		if (valor <= this.saldo) {
 			this.saldo = (this.saldo - valor);
-			System.out.println("Foi depositado para a conta Corrente o valor de: " + valor + " reais. Seu saldo atual é: "  + this.getSaldo());
-		}
-		else{
+			System.out.println("Foi depositado para a conta Corrente o valor de: " + valor
+					+ " reais. Seu saldo atual é: " + this.getSaldo());
+		} else {
 			System.out.println("Saldo insufiente. Transferência negada.");
 		}
 
 	}
-	public void verSaldo(){
+
+	public void verSaldo() {
 		System.out.println(getSaldo());
 	}
 }
